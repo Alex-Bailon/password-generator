1df075d13799304ab8a0dc3b1cf198c63de68016
Alex B <abailon949@gmail.com>
2020-02-20 17:28:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,41 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  let i = 0
   let lowercase = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`,`u`, `v`, `w`, `x`, `y`, `z`]
   let uppercase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`]
   let specialchar = [`!`, `@`, `#`, `$`, `%`, `&`, `*`, `-`]
+  let myArrays = [ lowercase, uppercase, specialchar ]
   let passlength = prompt(`Between 8 to 128 characters, how long would you like your password to be?`)
   let lowercaseConfirm = confirm(`Would you like lower-case letters for you password? (Presss cancel for "No" and okay for "Yes")`)
   let uppercaseConfirm = confirm(`Would you like upper-case letters for you password? (Presss cancel for "No" and okay for "Yes")`)
   let specialCharConfirm = confirm(`Would you like lower-case letters for you password? (Presss cancel for "No" and okay for "Yes")`)
   if ((lowercaseConfirm == true || uppercaseConfirm == true || specialCharConfirm == true) && passlength < 129 && passlength > 7 ){
-    confirm(`You would like ${ passlength } character password. It is ${ lowercaseConfirm } that you want lower-case letters. It is ${ uppercaseConfirm } that you want upper-case letters. It is ${ specialCharConfirm } that you want special characters.`)
+    let passwordConfirm = confirm(`You would like ${ passlength } character password. It is ${ lowercaseConfirm } that you want lower-case letters. It is ${ uppercaseConfirm } that you want upper-case letters. It is ${ specialCharConfirm } that you want special characters.`)   
+    if( passwordConfirm == true ) {
+      var passArray = []
+      if (lowercaseConfirm == true ){
+        passArray.push(0)
+      }
+      if (uppercaseConfirm == true ){
+        passArray.push(1)
+      }
+      if (specialCharConfirm == true ){
+        passArray.push(2)
+      }
+
+    }
   }
   else {
     alert(`Please ensure you select at least one character type and that your password length is between 8-128 chaaracters.`)
   }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
 
 }
 
