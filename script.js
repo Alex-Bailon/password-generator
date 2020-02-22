cf81a613221ce7db7595b3d97f2758ad102eef85
Alex B <abailon949@gmail.com>
2020-02-22 12:34:12 -0600

    Cleaned up code

--- script.js
+++ script.js
@@ -5,20 +5,19 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   let i = 0
   let password = []
+  let passArray = []
   let lowercase = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`,`u`, `v`, `w`, `x`, `y`, `z`]
   let uppercase = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`]
   let specialchar = [`!`, `@`, `#`, `$`, `%`, `&`, `*`, `-`]
   let nums = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
-  let passArray = []
   let passlength = prompt(`Between 8 to 128 characters, how long would you like your password to be?`)
   let lowercaseConfirm = confirm(`Would you like lower-case letters for you password? (Presss cancel for "No" and okay for "Yes")`)
   let uppercaseConfirm = confirm(`Would you like upper-case letters for you password? (Presss cancel for "No" and okay for "Yes")`)
-  let specialCharConfirm = confirm(`Would you like lower-case letters for you password? (Presss cancel for "No" and okay for "Yes")`)
+  let specialCharConfirm = confirm(`Would you like special characters letters for you password? (Presss cancel for "No" and okay for "Yes")`)
   let numsConfirm = confirm(`Would you like numbers for your password? (press cancel for "No and okay for "Yes"`)
   if ((lowercaseConfirm == true || uppercaseConfirm == true || specialCharConfirm == true || numsConfirm == true) && passlength < 129 && passlength > 7 ){
     let passwordConfirm = confirm(`You would like ${ passlength } character password. It is ${ lowercaseConfirm } that you want lower-case letters. It is ${ uppercaseConfirm } that you want upper-case letters. It is ${ specialCharConfirm } that you want special characters. It is ${ numsConfirm } that you want numbers.`)   
     if( passwordConfirm == true ) {
-      // var passArray = []
       if (lowercaseConfirm == true ){
         passArray.push(lowercase)
       }
@@ -37,10 +36,7 @@ function writePassword() {
             i++
 
       }
-      
-      console.log(password.join(""))
-      console.log(passArray)
-      console.log(window)
+      document.getElementById(`password`).textContent = password.join("")
     }
   
   }
